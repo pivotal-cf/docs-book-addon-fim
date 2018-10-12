8b6ef0f797ce67578316cc243bb22aa40d515657
radaeze <33758858+radaeze@users.noreply.github.com>
2018-10-12 13:48:44 -0700

    add v1.3 to pipes[#161098136]

--- redirects.rb
+++ redirects.rb
@@ -4,4 +4,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 # default product redirect
-r301 %r{/addon-fim/(?![\d-]+)(.*)}, "/addon-fim/1-3/$1"
+r301 %r{/addon-fim/(?![\d-]+)(.*)}, "/addon-fim/1-3/$1"r301 
