2b459f157f69b9dd3c9b8dbb54ac3fb8f1b0a19d
Samia Nneji <snneji@pivotal.io>
2019-12-02 16:57:03 +0000

    Add redirect configuration.html -> installing.html for 2.0 [#169957053]

--- redirects.rb
+++ redirects.rb
@@ -5,3 +5,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # default product redirect
 # r301 %r{/addon-fim/(?![\d-]+)(.*)}, "/addon-fim/1-3/$1"
+
+r301 %r{/addon-fim/2-0/configuration.html}, "/addon-fim/2-0/installing.html"
