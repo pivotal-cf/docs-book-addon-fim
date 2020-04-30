dd9d84647bf1f1e114332e2d50b789521d68265f
Miranda Limonczenko <mlimonczenko@pivotal.io>
2020-04-30 11:40:52 -0700

    cherry-pick commit from master

--- redirects.rb
+++ redirects.rb
@@ -3,8 +3,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# default product redirect
-# r301 %r{/addon-fim/(?![\d-]+)(.*)}, "/addon-fim/1-3/$1"
-
 # Redirect from old configuration topic to new combined installing and configuring topic
 r301 %r{/addon-fim/2-0/configuration.html}, "/addon-fim/2-0/installing.html"
