811a91e635993e45dae8e4faf3532a544ea2b2d5
Samia Nneji <snneji@pivotal.io>
2020-06-16 16:00:57 +0100

    Make master match edge [#172373591]

--- redirects.rb
+++ redirects.rb
@@ -6,4 +6,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 # NOTE: Redirect to the latest version now lives in redirects.rb file inside docs-book-pcfservices
 
 # Redirect from old configuration topic to new combined installing and configuring topic
-r301 %r{/addon-fim/2-0/configuration.html}, "/addon-fim/2-0/installing.html"
+r301 %r{/addon-fim/([\d-]+)/configuration.html}, "/addon-fim/$1/installing.html"
