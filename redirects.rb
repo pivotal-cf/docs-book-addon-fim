a5a3cf5a3477342d101e3aa9f171ac425a2ac816
animatedmax <mhufnagel@pivotal.io>
2022-03-11 13:29:05 -0800

    [#181500021] add redirect to docs.vmware.com for v2.1

--- redirects.rb
+++ redirects.rb
@@ -3,6 +3,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect from old configuration topic to new combined installing and configuring topic
-
+r301 %r{/addon-fim/2-1/(.*)}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/2.1/fim/GUID-index.html"
 
