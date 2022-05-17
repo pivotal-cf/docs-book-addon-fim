d0379c4be1882471d1e522c995e1db0ec5b21a2f
snneji <snneji@vmware.com>
2022-05-17 16:11:17 +0100

    Fix redirects for FIM [#182195205]

--- redirects.rb
+++ redirects.rb
@@ -3,5 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/addon-fim/2-1/(.*)}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/2.1/fim/GUID-index.html"
-
+r301 %r{/addon-fim/2-1/(.*)}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/2.1/fim/GUID-$1"
