34b7a5ab02cd3a3b85d8d12769f68b34af72e4b1
Samia Nneji <snneji@vmware.com>
2023-05-25 12:09:14 +0100

    [#185257044] Add redirect to VMware PDF

--- redirects.rb
+++ redirects.rb
@@ -3,5 +3,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect from old configuration topic to new combined installing and configuring topic
-r301 %r{/addon-fim/2-0/configuration.html}, "/addon-fim/2-0/installing.html"
+# Redirect from docs.pivotal.io/addon-fim/1-4/ to docs.vmware.com FIM PDF
+
+r301  %r{/addon-fim/1-4}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/1.4/fim-for-vmware-tanzu-1-4.pdf"
