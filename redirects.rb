6679aab0bfc572c1a0570e280aca6561375071e1
Samia Nneji <snneji@vmware.com>
2023-05-25 11:58:54 +0100

    [#185257044] Correct version in redirect

--- redirects.rb
+++ redirects.rb
@@ -3,9 +3,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect from old configuration topic to new combined installing and configuring topic
-r301 %r{/addon-fim/2-0/configuration.html}, "/addon-fim/2-0/installing.html"
-
 # Redirect from docs.pivotal.io/addon-fim/2-0/ to docs.vmware.com FIM PDF
 
-r301  %r{/addon-fim/2-0}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/2.0/fim-for-vmware-tanzu-2-0.pdf"
+r301  %r{/addon-fim/1-2}, "https://docs.vmware.com/en/File-Integrity-Monitoring-for-VMware-Tanzu/1.2/fim-for-vmware-tanzu-1-2.pdf"
